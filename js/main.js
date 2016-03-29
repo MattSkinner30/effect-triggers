2c1615b9d4542ac579de127c6e9dc8ab52cdd95b
MattSkinner30 <MattSkinner@Matts-MacBook-Pro.local>
2016-03-29 17:18:21 -0400

    Added second item

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$('.btm-show-hide').on('click', funciton (e) {
-  $(this).toggleClass('.show');
+var $body = $ ('body');
+
+$body.on('click', '.btn-show-hide', function () {
+  $('.box').toggleClass('show');
+});
+
+$body.on('click', '.btn-move', function () {
+  $('.diamond').toggleClass('move');
 });
