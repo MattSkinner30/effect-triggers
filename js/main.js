2840e9cf0357c021feca9a0789efe4af393895ef
MattSkinner30 <MattSkinner@Matts-MacBook-Pro.local>
2016-03-29 21:21:43 -0400

    Fixing Errors

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-var $body = $ ('body');
+var $body = $('body');
+var $btn1 = $('.btn-show-hide');
+var $btn2 = $('.btn-move');
+var $btn3 = $('.btn-collapse-expand');
+var $btn4 = $('.btn-btn-bounce');
+var $btn5 = $('.btn-append');
+var $list = $('.list');
+var $bounce = $('.btn-bounce');
 
 $body.on('click', '.btn-show-hide', function () {
   $('.box').toggleClass('show');
@@ -6,4 +13,20 @@ $body.on('click', '.btn-show-hide', function () {
 
 $body.on('click', '.btn-move', function () {
   $('.diamond').toggleClass('move');
+});
+
+$body.on('click', '.btn-collapse-expand', function () {
+  $('.panel').toggleClass('collapse');
+});
+
+$body.on('click', '.btn-bounce', function () {
+  $('.circle').addClass('bounce');
+});
+
+$body.on('animationend', '.circle', function () {
+  $(this).removeClass('bounce');
+});
+
+$body.on('click', '.btn-append', function () {
+  $('.list').append('<li>New List Item</li>');
 });
